78d12b1614de835279ee5a6fdfc9f0b0a00ca34d
Maszlag Norbert <norbi21um@gmail.com>
2022-02-16 14:39:43 +0100

    Backend project létrehozása
    Spring initializr-rel a  Spring boot projekt  létrehozása.

---
+++ backend/spring-boot-realestate/src/main/java/com/realestate/springbootrealestate/SpringBootRealestateApplication.java
@@ -0,0 +1,13 @@
+package com.realestate.springbootrealestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRealestateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRealestateApplication.class, args);
+	}
+
+}
---
+++ backend/spring-boot-realestate/src/test/java/com/realestate/springbootrealestate/SpringBootRealestateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.springbootrealestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRealestateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
